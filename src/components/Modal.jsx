2f845c8655f536b6c9cd6e14a87741801ca55739
GeorgeUsynin <angiejo03@gmail.com>
2022-11-07 18:33:37 +0300

    add context

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,10 @@
-import React, { useState, useImperativeHandle, forwardRef } from 'react';
+import React, { useState, useImperativeHandle, forwardRef, createContext, useContext } from 'react';
 import { createPortal } from 'react-dom';
 import './modal.css';
+
+const ModalContext = createContext({
+    closeModal: () => {},
+});
 
 const modal_root_el = document.createElement('div');
 modal_root_el.id = 'modal_root';
@@ -18,15 +22,22 @@ const Modal = forwardRef(({ children }, ref) => {
     if (!is_modal_visible) return null;
 
     if (modal_root_el) {
-        return createPortal(<div className='modal'>{children}</div>, modal_root_el);
+        return createPortal(
+            <ModalContext.Provider value={{ closeModal: () => setIsModalVisible(false) }}>
+                <div className='modal'>{children}</div>
+            </ModalContext.Provider>,
+            modal_root_el
+        );
     }
 });
 
 const Header = ({ title }) => {
+    const { closeModal } = useContext(ModalContext);
+
     return (
         <header>
             {title}
-            <span onClick={() => setIsModalVisible(false)}>x</span>
+            <span onClick={() => closeModal()}>x</span>
         </header>
     );
 };
